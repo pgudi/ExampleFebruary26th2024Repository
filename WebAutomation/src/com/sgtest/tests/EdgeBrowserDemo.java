193a488b93f3b54713398f3478859caa6930a539
pgudi <prabhakargudi@gmail.com>
2024-04-22 10:23:08 +0530

    Edge Browser

---
+++ WebAutomation/src/com/sgtest/tests/EdgeBrowserDemo.java
@@ -0,0 +1,49 @@
+package com.sgtest.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowserDemo {
+
+	public static WebDriver oBorwser=null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigateURL();
+		closeApplication();
+	}
+	
+	private static void launchBrowser()
+	{
+		try
+		{
+			oBorwser=new EdgeDriver();
+		}catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void navigateURL()
+	{
+		try
+		{
+			oBorwser.get("http://localhost/login.do");
+			Thread.sleep(5000);
+		}catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void closeApplication()
+	{
+		try
+		{
+			oBorwser.quit();
+		}catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
